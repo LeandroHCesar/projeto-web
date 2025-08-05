c423140fee029ca642146a0e2acab3cd6358e923
Leandro-PC <leandro-pitstop@hotmail.com>
2025-08-05 18:29:53 -0300

    feat: Estrutura inicial do projeto

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Importa o objeto 'meuModulo' do arquivo 'meu-modulo.js'
+import meuModulo from "./modules/meu-modulo.js";
+
+console.log("O arquivo main.js foi carregado com sucesso.");
+
+// Chama a função 'digaOla' que está dentro do objeto importado
+meuModulo.digaOla();
---
+++ src/modules/meu-modulo.js
@@ -0,0 +1,8 @@
+const meuModulo = {
+  digaOla: () => {
+    console.log("Olá! Esta mensagem veio do 'meu-modulo.js'.");
+  },
+};
+
+// Exporta o objeto para que ele possa ser importado em outros arquivos
+export default meuModulo;
